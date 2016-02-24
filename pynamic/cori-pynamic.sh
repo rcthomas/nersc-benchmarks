af2321dd691ddf3563827c9d4025a4453e51fc17
R. C. Thomas <rcthomas@lbl.gov>
2016-02-23 20:21:58 -0800

    afterok->afterany

--- pynamic/cori-pynamic.sh
+++ pynamic/cori-pynamic.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-job_id=`sbatch                              cori-pynamic-150-common.sh   | awk '{ print $NF }'`
+job_id=`sbatch                               cori-pynamic-150-common.sh   | awk '{ print $NF }'`
 echo $job_id
 
-job_id=`sbatch --dependency=afterok:$job_id cori-pynamic-150-datawarp.sh | awk '{ print $NF }'`
+job_id=`sbatch --dependency=afterany:$job_id cori-pynamic-150-datawarp.sh | awk '{ print $NF }'`
 echo $job_id
 
-job_id=`sbatch --dependency=afterok:$job_id cori-pynamic-150-project.sh  | awk '{ print $NF }'`
+job_id=`sbatch --dependency=afterany:$job_id cori-pynamic-150-project.sh  | awk '{ print $NF }'`
 echo $job_id
 
-job_id=`sbatch --dependency=afterok:$job_id cori-pynamic-150-scratch.sh  | awk '{ print $NF }'`
+job_id=`sbatch --dependency=afterany:$job_id cori-pynamic-150-scratch.sh  | awk '{ print $NF }'`
 echo $job_id
 
-job_id=`sbatch --dependency=afterok:$job_id cori-pynamic-150-tmpfs.sh    | awk '{ print $NF }'`
+job_id=`sbatch --dependency=afterany:$job_id cori-pynamic-150-tmpfs.sh    | awk '{ print $NF }'`
 echo $job_id
 
 squeue -o "%10i %10u %10a %30j %10P %10q %5D %10l %10M %2t %S" -u $USER

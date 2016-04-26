b1a8e29b5ab0b559a7929ef4a97d0e6cdfbb84d6
R. C. Thomas <rcthomas@lbl.gov>
2016-04-26 09:36:11 -0700

    More verbose about what's being deleted.

--- pynamic/purge-output.sh
+++ pynamic/purge-output.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 # Remove any slurm-*.out files older than a few days.
-find . -name "slurm-*.out" -mtime +3 -exec rm -f {} \;
+find . -name "slurm-*.out" -mtime +3 -exec rm -vf {} \;
